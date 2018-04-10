62476124d46d12db38795597643e2fb137c3f32c
amaurs <amaury.gtz@gmail.com>
2018-04-10 18:58:21 -0500

    Features can be updated.

---
+++ javascript/util.js
@@ -0,0 +1,115 @@
+/**
+This function is a color map for the MADMex classes.
+**/
+function getColor(tag) {
+    var text;
+    switch(tag) {
+    case 0:
+        text = "#000000";
+        break;
+    case 1:
+        text = "#005100";
+        break;
+    case 2:
+        text = "#007e00";
+        break;
+    case 3:
+        text = "#003c00";
+        break;
+    case 4:
+        text = "#aaaa00";
+        break;
+    case 5:
+        text = "#aa8000";
+        break;
+    case 6:
+        text = "#8baa00";
+        break;
+    case 7:
+        text = "#ffb265";
+        break;
+    case 8:
+        text = "#00d900";
+        break;
+    case 9:
+        text = "#aa007f";
+        break;
+    case 10:
+        text = "#ff55ff";
+        break;
+    case 11:
+        text = "#ff557f";
+        break;
+    case 12:
+        text = "#ff007f";
+        break;
+    case 13:
+        text = "#ff55ff";
+        break;
+    case 14:
+        text = "#aaffff";
+        break;
+    case 15:
+        text = "#00ffff";
+        break;
+    case 16:
+        text = "#55aaff";
+        break;
+    case 17:
+        text = "#e29700";
+        break;
+    case 18:
+        text = "#bd7e00";
+        break;
+    case 19:
+        text = "#966400";
+        break;
+    case 20:
+        text = "#a2ecb1";
+        break;
+    case 21:
+        text = "#c46200";
+        break;
+    case 22:
+        text = "#aa5500";
+        break;
+    case 23:
+        text = "#6d3600";
+        break;
+    case 24:
+        text = "#00aa7f";
+        break;
+    case 25:
+        text = "#008a65";
+        break;
+    case 26:
+        text = "#005941";
+        break;
+    case 27:
+        text = "#e9e9af";
+        break;
+    case 28:
+        text = "#0b5923";
+        break;
+    case 29:
+        text = "#faff98";
+        break;
+    case 30:
+        text = "#4d1009";
+        break;
+    case 31:
+        text = "#000ff0";
+        break;
+    case 32:
+        text = "#00007f";
+        break;
+    case 33:
+        text = "#000000";
+        break;
+    case 34:
+        text = "#fef7ff";
+        break;
+    
+  }
+  return text;
+}
